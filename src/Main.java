fa2849f5f2863e94a999d95573276911c69dc89d
Karuna Rabha <karrabha@publicisgroupe.net>
2023-12-06 11:37:44 +0530

    second commit 6dec

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,34 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    BufferedReader buff;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    InputStreamReader isr;
+    public Main() {
+        if(isr == null){
+            isr = new InputStreamReader(System.in);
         }
+        if(buff == null){
+            buff = new BufferedReader(isr);
+        }
+    }
+    int selectedBank;
+
+    public static void main(String[] args){
+      Main obj = new Main();
+     System.out.println("Welcome to IBS\nPlease select your bank\n1. ICICI\n2. HDFC\n3. SBI\n4. AXIS\n5. IDFC");
+     InputStreamReader isr = new InputStreamReader(System.in);
+     BufferedReader buff = new BufferedReader(isr);
+     try{
+         obj.selectedBank = Integer.parseInt(obj.buff.readLine());
+         System.out.println("Customer Selected" + obj.selectedBank);
+     }
+     catch (IOException e){
+         e.printStackTrace();
+     }
+
+
     }
 }
